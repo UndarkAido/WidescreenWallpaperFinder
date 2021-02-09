edd8e2612166aee1316689831ca18aa5bf90db2d
Aido <7421794+UndarkAido@users.noreply.github.com>
2021-02-09 10:52:04 -0600

    Create WidescreenWallpaperFinder.meta.js

---
+++ WidescreenWallpaperFinder.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Widescreen Wallpaper Finder
+// @namespace    http://theundarkpixel.com/
+// @version      1.0
+// @description  Filter images on a page by a minimum width
+// @author       UndarkAido
+// @source       https://github.com/UndarkAido/WidescreenWallpaperFinder
+// @updateURL    https://raw.githubusercontent.com/UndarkAido/WidescreenWallpaperFinder/master/WidescreenWallpaperFinder.meta.js
+// @downloadURL  https://raw.githubusercontent.com/UndarkAido/WidescreenWallpaperFinder/master/WidescreenWallpaperFinder.user.js
+// @supportURL   https://github.com/UndarkAido/WidescreenWallpaperFinder/issues
+// @include      https://www.pidgi.net/*
+// @include      https://wow.gamepedia.com/*
+// @include      https://wall.alphacoders.com/*
+// @include      http://www.thevideogamegallery.com/*
+// @include      https://press.activision.com/*
+// @include      https://blizzard.gamespress.com/*
+// @include      https://www.pexels.com/*
+// @include      https://commons.wikimedia.org/*
+// @include      https://www.deviantart.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
